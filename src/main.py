1bd6736e3acadf952f9d126e38d086e4926a065b
ANDS14 <andres.ruizcalvo@icloudcom>
2024-08-26 17:22:06 +0200

    Add README with instructions

--- src/main.py
+++ src/main.py
@@ -18,18 +18,13 @@ if __name__ == '__main__':
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
 
-    print("Training set shape: ", X_train.shape)
-    print("Test set shape: ", X_test.shape)
-    
     dict_models = search_spaces_dict_models()
-
-    print(dict_models)
 
     create_folder_structure()
 
     results, best_model_clf, best_model_name = training(dict_models, X,X_train, X_test, y_train,y_test)
 
-    print(results)
+    print(f"Best Model: {best_model_name}")
 
     load_display_best_model(best_model_name)
 
